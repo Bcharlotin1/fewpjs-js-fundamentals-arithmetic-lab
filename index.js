6a69270477768bb16ef72af6f61429973265a9aa
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-13 12:44:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1 = 2
+var num2 = 31
+var multiply = num1 * num2
+var random = Math.floor(Math.random() * 10) + 1;
+ /* Math.random() returns a number between 0 and 0.999..., which we multiply by 10 to give us a number between 0 and 9.999.... We then pass that number to Math.floor(), which returns an integer between 0 and 9. That's one less than the desired range (1 to 10), so we add one at the end of the equation. Try it out in the JS console! */
+var num3 = 16
+var num4 = 6
+var mod = num3 % num4
+
+var max = Math.max(1, 2, 3, 4, 20)
+
